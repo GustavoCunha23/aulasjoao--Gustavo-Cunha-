8651121ae79617c349e376f25b20ada27634166a
GustavoCunha23 <gustavo.cunha.oliveira25@outlook.com>
2022-08-08 21:58:26 -0300

    aula 01 - Introdução ao JS

---
+++ PMII/aula01/teste.js
@@ -0,0 +1,12 @@
+var a = 3 // Declarando Variavel com var
+let b = 4 // Declarando Variavel com let
+console.log('Conteúdo de a: ', a, 'conteúdo de b: ', b)
+
+
+var a = 30 // var 
+b = 40
+
+console.log(a,b)
+
+a = 300
+b = 400
